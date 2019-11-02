0029c5404447f2dc394db60d3d7b8734859b5732
cfvbnm <57104036+cfvbnm@users.noreply.github.com>
2019-11-02 12:01:45 +0100

    Add files via upload

---
+++ Cw3/Car.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cw3
+{
+    class Car
+    {
+        public double pojemnoscSilnika;
+        public string marka;
+
+        public Car()
+        {
+
+        }
+
+        public Car(double pojemnoscSilnika, string marka)
+        {
+            this.pojemnoscSilnika = 3.5;
+            this.marka = "Jakas Marka";
+        }
+    }
+}
---
+++ Cw3/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cw3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Car auto = new Car(20, "Jakas marka");
+            Console.WriteLine(auto.pojemnoscSilnika);
+            Console.WriteLine(auto.marka);
+
+            Console.ReadLine();
+
+        }
+    }
+}
